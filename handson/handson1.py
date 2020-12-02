8ef7ca5cb9171b6559255771d6975a9aa10506af
PriyankaSahu1 <75246028+PriyankaSahu1@users.noreply.github.com>
2020-12-02 16:37:19 +0530

    Update handson1.py

--- handson/handson1.py
+++ handson/handson1.py
@@ -1,1 +1,19 @@
 
+# handson 1
+#Write a program to Add, Subtract, Multiply, and Divide 2 numbers
+
+
+x=int(input('enter integer value for x:'))
+y=int(input('enter integer value for y:'))
+#Add
+print('addition:',x+y)
+
+#subtract
+print('subtraction:',x-y)
+
+#Multiply
+print('Multiplication:',x*y)
+
+#Multiply
+print('Division:',x/y)
+
