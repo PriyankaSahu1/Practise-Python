df8ca63c336fe24b676cb39b789fd96c28f4bbd0
PriyankaSahu1 <75246028+PriyankaSahu1@users.noreply.github.com>
2020-12-03 16:38:23 +0530

    Create handson7.py

---
+++ handson/handson7.py
@@ -0,0 +1,22 @@
+#handson7
+#Create a list with at least 10 elements having integer values in it;
+       #Print all elements
+       #Perform slicing operations
+       #Perform repetition with * operator
+       #Perform concatenation with other list.
+       
+myList=[1,2,3,4,5,6,7,8,9,10]
+
+#Print all elements
+print('The list elements are\n',myList)
+
+#Perform slicing operations
+print('slicing operation in list from 2 to 7:\n',myList[2:7])
+
+#Perform repetition with * operator
+print('repetation of list:\n',myList*3)
+
+#Perform concatenation with other list.
+myList2=["one","two","three"]
+print('concatinated two list:',myList+myList2)
+
