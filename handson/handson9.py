d804a373129cac7a794a13d8562f9b2c0078e4b2
PriyankaSahu1 <75246028+PriyankaSahu1@users.noreply.github.com>
2020-12-03 16:40:22 +0530

    Create handson9.py

---
+++ handson/handson9.py
@@ -0,0 +1,12 @@
+#handson9
+#Python Numbers	Write program to Add, Subtract, Multiply, Divide 2 Complex numbers.
+
+x=10+5j
+y=12-6j
+
+print('operations on complex numbers:\n',x,y)
+
+print('add:',x+y)
+print('subtract:',x-y)
+print('multiply:',x*y)
+print('divide:',x/y)
