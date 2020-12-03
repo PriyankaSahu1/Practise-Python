9720ef1727f8ace296c1abb233a1e4015a5720ef
PriyankaSahu1 <75246028+PriyankaSahu1@users.noreply.github.com>
2020-12-03 16:39:41 +0530

    Create handson8.py

---
+++ handson/handson8.py
@@ -0,0 +1,17 @@
+#handson8
+
+myTuple=(1,2,3,4,5,6,7,8,9,10)
+
+#Print all elements
+print('The tuple elements are\n',myTuple)
+
+#Perform slicing operations
+print('slicing operation in tuple from 5 to 7:\n',myTuple[5:7])
+
+#Perform repetition with * operator
+print('repetation of tuple:\n',myTuple*2)
+
+#Perform concatenation with other list.
+myTuple2=("one","two","three")
+print('concatinated two tuple:',myTuple+myTuple2)
+
