3946ee4382b6ce923ed9c492735b0d5029dc68b2
PriyankaSahu1 <75246028+PriyankaSahu1@users.noreply.github.com>
2020-12-03 16:41:29 +0530

    Create handson10.py

---
+++ handson/handson10.py
@@ -0,0 +1,20 @@
+#handson10
+#1Assignment Operator	Using assignment operators, perform following operations
+#Addition, Substation, Multiplication, Division, Modulus, Exponent and Floor division operations
+
+x=5
+print(x)
+x+=3
+print('add:',x)
+x-=3
+print('sub:',x)
+x*=3
+print('mul:',x)
+x/=3
+print('div:',x)
+x%=10
+print('mod:',x)
+x**=3
+print('exponent:',x)
+x//=3
+print('floor div:',x)
