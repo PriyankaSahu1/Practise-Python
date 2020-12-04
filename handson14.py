206c00c178db630d320b7412898add377c564cd6
PriyankaSahu1 <priyankasahu101@gmail.com>
2020-12-04 10:10:25 +0000

    commit from ubuntu

---
+++ handson14.py
@@ -0,0 +1,8 @@
+list1=(101,102,103,104,105,106,107,108,109,110)
+print(list1)
+list2=("sarah","alex","anthony","mary","maria","tony","klein","kale","kevin","kelvin")
+print(list2)
+
+index=int(input("ënetr the index to check the details: "))
+print(list1[index],list2[index])
+
